1c4dd997b1db94bac15b320faca4623a9bd85d18
King'ori Maina <j@kingori.co>
2013-07-01 18:35:28 +0300

    Add TVs

---
+++ _build/data/transport.tvs.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Package TVs (Template Variables)
+ * @package modx
+ * @subpackage build
+ */
+
+$tvs = array();
+
+
+// $tvs[$i] = $modx -> newObject('modTemplateVar');
+// $tvs[$i] -> fromArray(array(
+//     'id' => 1,
+//     'source' => 2,
+//     'name' => 'exampletv',
+//     'description' => 'Description for TV',
+//     'caption' => 'Caption for TV',
+//     'type' => 'image',
+//     'display' => 'default',
+//     'default_text' => 'example.jpg',
+// ), '', true, true, false); $i++;
+
+
+return $tvs;
