a92fd0d0071231af6ff1b1413d54cd87770c439c
King'ori Maina <j@kingori.co>
2013-07-01 18:34:55 +0300

    Add resource and resource groups

---
+++ _build/data/transport.resourcegroups.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Package Resource Groups
+ * @package modx
+ * @subpackage build
+ */
+
+$i = 1;
+$resourcegroups = array();
+
+
+// $resourcegroups[$i] = $modx -> newObject('modResourceGroup');
+// $resourcegroups[$i] -> fromArray(array(
+//     'id' => 1,
+//     'name' => 'Example Resource Group',
+//     'private_memgroup' => 0,
+//     'private_webgroup' => 0,
+// ), '', true, true, false); $i++;
+
+
+
+return $resourcegroups;
---
+++ _build/data/transport.resources.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Package Resources
+ * @package modx
+ * @subpackage build
+ */
+
+// Initialize
+$i = 1;
+$resources = array();
+
+
+// EXAMPLE RESOURCE
+// $resources[$i] = $modx -> newObject('modResource');
+// $resources[$i] -> fromArray(array(
+//                         'id' => 1,
+//                         'pagetitle' => 'Example Resource',
+//                         'parent' => 0,
+//                         'isfolder' => 0,
+//                         'hidemenu' => 0,
+//                         'alias' => 'example-resource',
+//                         'published' => 1,
+//                         'searchable' => 1,
+//                         'template' => 1,
+//                         'menuindex' => 0,
+//                         'longtitle' => 'Example Resource',
+//                         'description' => 'Example Resource Template',
+//                         'introtext' => 'Example Resource Template',
+// ), '', true, true, false); $i++;
+
+
+
+return $resources;
