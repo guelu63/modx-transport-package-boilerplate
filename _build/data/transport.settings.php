f055b2bf5dcfb461ef3e187ac288bca92dbecfb6
King'ori Maina <j@kingori.co>
2013-07-01 18:35:10 +0300

    Add settings

---
+++ _build/data/transport.settings.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Package System Settings
+ * @package modx
+ * @subpackage build
+ */
+
+$settings = array();
+
+
+// $settings['example.example'] = $modx -> newObject('modSystemSetting');
+// $settings['example.example'] -> fromArray(array(
+//     'key' => 'example.key',
+//     'value' => 'example.key',
+//     'xtype' => 'textfield',
+//     'namespace' => 'modx-transport-package-boilerplate',
+//     'area' => 'example.area',
+// ), '', true, true, false);
+
+
+return $settings;
